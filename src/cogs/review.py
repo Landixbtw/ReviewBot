6db7ce3302047a2932cd80754b76fbc19ebe5db5
landixbtw <landixbtw@gmail.com>
2024-01-22 21:10:52 +0100

    Initial Commit

---
+++ src/cogs/review.py
@@ -0,0 +1,60 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+import datetime
+import logging
+
+
+bot = commands.Bot(command_prefix="&&", intents=discord.Intents.all())
+
+
+class review(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @app_commands.command(name="review", description="Leave a review")
+    @app_commands.describe(bought="What did you buy ?")
+    @app_commands.describe(description="Leave a written review")
+    @app_commands.choices(product=[
+        app_commands.Choice(name='1 Star', value='⭐'),
+        app_commands.Choice(name='2 Stars', value='⭐⭐'),
+        app_commands.Choice(name='3 Stars', value='⭐⭐⭐'),
+        app_commands.Choice(name='4 Stars', value='⭐⭐⭐⭐'),
+        app_commands.Choice(name='5 Stars', value='⭐⭐⭐⭐⭐'),
+    ])
+    @app_commands.choices(service=[
+        app_commands.Choice(name='1 Star', value='⭐'),
+        app_commands.Choice(name='2 Stars', value='⭐⭐'),
+        app_commands.Choice(name='3 Stars', value='⭐⭐⭐'),
+        app_commands.Choice(name='4 Stars', value='⭐⭐⭐⭐'),
+        app_commands.Choice(name='5 Stars', value='⭐⭐⭐⭐⭐'),
+    ])
+    
+    async def review(self, interaction: discord.Interaction, bought: str, description: str, product: app_commands.Choice[str], service: app_commands.Choice[str]):
+        
+        print(f"{product}")
+        print(f"{service}")    
+        
+        now = datetime.datetime.now()
+        current_time = now.strftime('%H:%M:%S')   
+        try:
+            embed = discord.Embed(title=f"Review from {interaction.user.name}", description=f"**Product:** {bought}", color=0x3eb1d0) # Green color
+            embed.set_author(name="Review Bot", url="https://discord.com/api/oauth2/authorize?client_id=1198686544341508218&permissions=8&scope=bot", icon_url="https://cdn.discordapp.com/attachments/1192482504888827914/1198738729595043850/2EN2srv.jpg?ex=65bfff70&is=65ad8a70&hm=9dddc51f44009a9df96aaf9694f04124f1a933e9297297a64f9b800fac55f527&") # invite with admin perms
+            #embed.add_field(name="", value="\u200b", inline=False)  # Empty field for description
+            embed.add_field(name="Review: ", value=f"{description}", inline=False)
+            fields = [] # field list for products and service
+            fields.append((f"product: {product.name}", f"{product.value}", True)) # Product field added
+            fields.append((f"service: {service.name}", f"{service.value}", True)) # Service field added
+            for name, value, inline in fields:
+                embed.add_field(name=name, value=value, inline=inline) # I don't even know. ##! Don't touch it it works
+            embed.set_footer(text=f"{current_time}")
+
+            await interaction.response.send_message(embed=embed)
+
+        except Exception as err:
+            logging.warning(err)
+            
+            
+async def setup(bot):
+    await bot.add_cog(review(bot))
+    print("review cog geladen ✔️")
---
+++ src/main.py
@@ -0,0 +1,62 @@
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+import logging
+import datetime
+
+
+load_dotenv()
+token = str(os.getenv("TOKEN"))
+
+bot = commands.Bot(command_prefix="&&", intents=discord.Intents.all())
+
+# logs_dir = "./Logs"
+# if not os.path.exists(logs_dir):
+#     print("Made Logs Folder and file!")
+#     os.makedirs(logs_dir)
+# # # Logging Handler to log info
+# handler = logging.FileHandler(
+#     filename="./Logs/discord.log", encoding="utf-8", mode="w"
+# )
+
+
+class bot(commands.Bot):
+    def __init__(
+        self,
+    ):
+        super().__init__(command_prefix="&&", intents=discord.Intents.all())
+
+
+    
+    async def setup_hook(self):
+        print("loading cogs ...")
+        for file in os.listdir("./cogs"):  # lists all the cog files inside the cog folder. (for raspberry /home/username/WarnBot/src/cogs)
+            if file.endswith(".py"):  # It gets all the cogs that ends with a ".py".
+                try:
+                    name = file[:-3]  # It gets the name of the file removing the ".py"
+                    await bot.load_extension(f"cogs.{name}")  # This loads the cog.
+                except Exception as e:
+                    print(f"error: {e}")
+        
+    async def on_ready(self):
+        print(f"{bot.user.name} is ready to rumble!")
+        print("Published by Moritz Henri Richard Reiswaffel III")
+        try:
+            synced = await self.tree.sync()
+            print(f"Synced {len(synced)} commands!")
+        except Exception as e:
+            print(e)
+        
+        print("------------------------------")
+        await bot.change_presence(
+            activity=discord.Activity(
+                type=discord.ActivityType.watching, name=f"{bot.command_prefix}help"
+            )
+        )
+
+bot = bot()
+
+bot.run(token,)
+
+
