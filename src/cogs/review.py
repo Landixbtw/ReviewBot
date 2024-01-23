6892ff100e27cef7ad4f0f472fd1ffe250fe16bd
landixbtw <landixbtw@gmail.com>
2024-01-23 11:41:50 +0100

    changes

--- src/cogs/review.py
+++ src/cogs/review.py
@@ -41,13 +41,13 @@ class review(commands.Cog):
             embed = discord.Embed(title=f"Review from {interaction.user.name}", description=f"**Product:** {bought}", color=0x3eb1d0) # Green color
             embed.set_author(name="Review Bot", url="https://discord.com/api/oauth2/authorize?client_id=1198686544341508218&permissions=8&scope=bot", icon_url="https://cdn.discordapp.com/attachments/1192482504888827914/1198738729595043850/2EN2srv.jpg?ex=65bfff70&is=65ad8a70&hm=9dddc51f44009a9df96aaf9694f04124f1a933e9297297a64f9b800fac55f527&") # invite with admin perms
             #embed.add_field(name="", value="\u200b", inline=False)  # Empty field for description
-            embed.add_field(name="Review: ", value=f"{description}", inline=False)
+            embed.add_field(name="**Review:** ", value=f"{description}", inline=False)
             fields = [] # field list for products and service
             fields.append((f"product: {product.name}", f"{product.value}", True)) # Product field added
             fields.append((f"service: {service.name}", f"{service.value}", True)) # Service field added
             for name, value, inline in fields:
                 embed.add_field(name=name, value=value, inline=inline) # I don't even know. ##! Don't touch it it works
-            embed.set_footer(text=f"{current_time}")
+            embed.set_footer(text=f"{current_time}", icon_url="")
 
             await interaction.response.send_message(embed=embed)
 
